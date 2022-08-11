1076411ad23b286578c2181b74bd74f1bc0c6b64
Ivan583 <ivanych2293@gmail.com>
2022-08-11 12:40:33 +0300

    Передача списка в компонент

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import PostItem from './components/PostItem';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: "Hotel", body: "We are living in the Heart-Break Hotel"},
+    {id: 2, title: "Sky", body: "I am eagle that rides in the breeze"},
+    {id: 3, title: "Forest", body: "He was a SteppenWolf"},
+  ]);
+
   return (
     <div className="App">
       <h1>My React App</h1>
-        <PostItem post={{id: 1, title: "Hotel", body: "We are living in the Heart-Break Hotel"}} />
+        {posts.map(post => 
+          <PostItem post={post} key={post.id} />
+          )}
     </div>
   );
 }
