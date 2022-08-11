1ca6d5b537fb516dc49449e56853807ef9971b53
Ivan583 <ivanych2293@gmail.com>
2022-08-11 14:58:05 +0300

    Добавление компонента формы

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,32 +9,14 @@ function App() {
     {id: 2, title: "Sky", body: "I am eagle that rides in the breeze"},
     {id: 3, title: "Forest", body: "He was a SteppenWolf"},
   ]);
-
-  const [post, setPost] = useState({title: '', body: ''});
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}]);
-    setPost({title: '', body: ''});
+ 
+  const createNewPost = newPost => {
+    setPosts([...posts, newPost]);
   };
 
   return (
     <div className="App">
-      <form>
-        <MyInput
-         value={post.title}
-         onChange={e => setPost({...post, title: e.target.value})}
-         type="text" 
-         placeholder="Заголовок поста" />
-
-        <MyInput
-         value={post.body}
-         onChange={e => setPost({...post, body: e.target.value})}        
-         type="text" 
-         placeholder="Описание поста" />
-
-        <MyButton onClick={addNewPost} >Create post</MyButton>
-      </form>
+      <PostForm create={createNewPost} />
 
       <PostList posts={posts} title="Posts List" />
     </div>
