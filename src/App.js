d0ccc8213ab128f7964d6a4b5b6d654df8bc4779
Ivan583 <ivanych2293@gmail.com>
2022-08-12 11:27:09 +0300

    Сортировка постов

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 
   const sortPosts = sort => {
     setSelectedSort(sort);
-    console.log(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
 
   return (
